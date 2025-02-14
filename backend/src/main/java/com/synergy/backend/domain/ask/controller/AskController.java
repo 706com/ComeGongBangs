fbca48d947364d1b6a3e7ce67703b36c70dc118b
706com <ehdgusjdbswl@gmail.com>
2025-02-14 21:11:38 +0900

    [Refactor] RESTful API 준수

--- backend/src/main/java/com/synergy/backend/domain/ask/controller/AskController.java
+++ backend/src/main/java/com/synergy/backend/domain/ask/controller/AskController.java
@@ -20,17 +20,17 @@ public class AskController {
     private final AskService askService;
 
     // 문의 생성 API
-    @PostMapping("/create")
+    @PostMapping("/")
     public BaseResponse<AskRes> createAsk(@RequestBody AskReq askReq, @AuthenticationPrincipal CustomUserDetails customUserDetails) throws BaseException{
         if(customUserDetails==null){
             throw new BaseException(BaseResponseStatus.NEED_TO_LOGIN);
         }
-
         AskRes response = askService.createAsks(askReq,customUserDetails.getIdx());
         return new BaseResponse<>(response);  // BaseResponse만 반환
    }
 
     // 문의 조회 API
+    //TODO : @RequestParam 은 필터링에 적합하고, 특정 자원 ID 가 있는경우 @PathVariable이 적합하다.
     @GetMapping("/list/read")
     public BaseResponse<List<AskRes>> getAsksList(@RequestParam Long productIdx,Integer page, Integer size, @AuthenticationPrincipal CustomUserDetails customUserDetails) throws BaseException {
         Long memberIdx = null;
