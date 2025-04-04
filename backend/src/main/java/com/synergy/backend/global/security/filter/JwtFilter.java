91c8eb1840d499bb000af17d9857674cab657b34
706com <ehdgusjdbswl@gmail.com>
2025-04-04 15:25:29 +0900

    [Refactor] 필터 코드 개선

--- backend/src/main/java/com/synergy/backend/global/security/filter/JwtFilter.java
+++ backend/src/main/java/com/synergy/backend/global/security/filter/JwtFilter.java
@@ -27,23 +27,22 @@ public class JwtFilter extends OncePerRequestFilter {
     private final BlackListTokenService blackListTokenService;
     private final String frontRedirectUrl;
 
+    // JWT 필터를 적용하지 않을 경로
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        String path = request.getRequestURI();
+        return path.startsWith("/login");
+//                || path.startsWith("/product/search");
+//                || path.startsWith("/product/detail/");
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-        String accessToken = null;
-        String refreshToken = null;
         //쿠키를 통한 요청 받기
-        if (request.getCookies() != null) {
-            for (Cookie cookie : request.getCookies()) {
-                if (cookie.getName().equals("JToken")) {
-                    accessToken = cookie.getValue();
-                }
-                if (cookie.getName().equals("RefreshToken")) {
-                    refreshToken = cookie.getValue();
-                }
-            }
-        }
+        String accessToken = getCookieValue(request,"JToken");
+        String refreshToken = getCookieValue(request,"RefreshToken");
 
         // JToken을 받지 못했으면 다음 필터로 넘기기.
         if (accessToken == null) {
@@ -51,79 +50,56 @@ public class JwtFilter extends OncePerRequestFilter {
             return;
         }
 
-        String token = accessToken;
+//        String token = accessToken;
 
-        if (jwtUtil.isExpired(token)) {
-            log.info("=====Access 토큰 만료=====");
+        // AccessToken 만료 체크
+        if (jwtUtil.isExpired(accessToken)) {
+            log.info("[JWT] =====AT 만료=====");
 
             // 블랙리스트에 등록되어 있으면 불법 토큰으로 판단
             if (blackListTokenService.checkBlackList(accessToken, refreshToken)) {
-                log.info("=====블랙리스트 토큰 적발 및 삭제 처리=====");
+                log.error("[JWT] =====블랙리스트 토큰 적발 및 삭제 처리=====");
 
                 // 불법 쿠키 삭제
-                Cookie deleteAccessTokenCookie = new Cookie("JToken", null);
-                deleteAccessTokenCookie.setMaxAge(0);
-                deleteAccessTokenCookie.setPath("/");
-                response.addCookie(deleteAccessTokenCookie);
+                deleteCookie(response,"JToken");
+                deleteCookie(response,"RefreshToken");
 
-                Cookie deleteRefreshTokenCookie = new Cookie("RefreshToken", null);
-                deleteRefreshTokenCookie.setMaxAge(0);
-                deleteRefreshTokenCookie.setPath("/");
-                response.addCookie(deleteRefreshTokenCookie);
-
-                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                response.setContentType("application/json");
-                PrintWriter out = response.getWriter();
-                out.print("{\"redirectUrl\": \"" + frontRedirectUrl + "/logout\"}");
-                out.flush();
-
-//                filterChain.doFilter(request, response);
+                // 로그아웃 강제 유도
+                sendLogoutPage(response);
                 return;
             }
 
-            if (refreshToken == null) { // 만료 + refresh 토큰이 없을 때
-                log.info("=====Refresh token이 없음=====");
-//                response.sendRedirect("");
-//                filterChain.doFilter(request, response);
-                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                response.setContentType("application/json");
-                PrintWriter out = response.getWriter();
-                out.print("{\"redirectUrl\": \"" + frontRedirectUrl + "/logout\"}");
-                out.flush();
+            // AccessToken 만료 + refreshToken이 없을 때
+            if (refreshToken == null) {
+                log.error("[JWT] =====RT 없음=====");
+
+                sendLogoutPage(response);
                 return;
             }
 
             String reIssuedAccessToken = refreshTokenService.reIssueAccessToken(refreshToken);
-            if (reIssuedAccessToken == null) { // client의 refresh token이 변조되었거나, 만료되었거나, 서버가 가지고있는 refreshtoken과 다르거나
-                log.info("=====Refresh token 이상 감지=====");
-                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                response.setContentType("application/json");
-                PrintWriter out = response.getWriter();
-                out.print("{\"redirectUrl\": \"" + frontRedirectUrl + "/logout\"}");
-                out.flush();
-//                filterChain.doFilter(request, response);
+            // client의 refreshToken이 변조되었거나, 만료되었거나, 서버가 가지고있는 refreshToken과 다를 때
+            if (reIssuedAccessToken == null) {
+                log.error("[JWT] =====RT 이상 감지=====");
+                sendLogoutPage(response);
+
                 return;
             }
 
-            token = reIssuedAccessToken;
-            Cookie cookie = new Cookie("JToken", token);
-            cookie.setHttpOnly(true);
-            cookie.setSecure(true);
-            cookie.setPath("/");
-            response.addCookie(cookie);
+            // RT로 인한 AT 재갱신
+            addCookie(response,"JToken",reIssuedAccessToken);
 
-            refreshToken = refreshTokenService.reIssueRefreshToken(refreshToken); //RTR 적용
-            Cookie reissuedRefreshToken = new Cookie("RefreshToken", refreshToken);
-            reissuedRefreshToken.setHttpOnly(true);
-            reissuedRefreshToken.setSecure(true);
-            reissuedRefreshToken.setPath("/");
-            response.addCookie(reissuedRefreshToken);
+            // RTR 전략으로 인한 RT 또한 재갱신
+            String reIssuedRefreshToken = refreshTokenService.reIssueRefreshToken(refreshToken); //RTR 적용
+            addCookie(response,"RefreshToken",reIssuedRefreshToken);
+
+            log.info("[JWT] =====AT,RT 재갱신 완료=====");
         }
 
-        //정상 토큰 및 만료시간 통과
-        Long idx = jwtUtil.getIdx(token);
-        String username = jwtUtil.getUsername(token);
-        String role = jwtUtil.getRole(token);
+        // 정상 토큰 및 만료시간 통과
+        Long idx = jwtUtil.getIdx(accessToken);
+        String username = jwtUtil.getUsername(accessToken);
+        String role = jwtUtil.getRole(accessToken);
 
         // 인증-인가용 임시 멤버 객체 생성
         Member member = new Member(idx, username, role);
@@ -131,11 +107,50 @@ public class JwtFilter extends OncePerRequestFilter {
         // 직접 CustomDetails 객체로 변환
         CustomUserDetails customUserDetails = new CustomUserDetails(member);
 
-        // ContextHolder에 미리 심어줌으로서, LoginFilter가 로그인 된 사용자라고 판명
+        // ContextHolder에 심어줌으로서, LoginFilter가 로그인 된 사용자라고 판명
         Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null,
                 customUserDetails.getAuthorities());
         SecurityContextHolder.getContext().setAuthentication(authToken);
 
         filterChain.doFilter(request, response);
+    }
+
+    // 쿠키 정보 획득
+    private String getCookieValue(HttpServletRequest request,String name){
+        if(request.getCookies()==null){
+            return null;
+        }
+        for(Cookie cookie : request.getCookies()){
+            if(name.equals(cookie.getName())){
+                return cookie.getValue();
+            }
+        }
+        return null;
+    }
+
+    // 쿠키 추가
+    private void addCookie(HttpServletResponse response, String name, String value){
+        Cookie cookie = new Cookie(name, value);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+    }
+
+    // 쿠키 삭제
+    private void deleteCookie(HttpServletResponse response, String name){
+        Cookie cookie = new Cookie(name, null);
+        cookie.setMaxAge(0);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+    }
+
+    // 로그아웃 강제 유도
+    private void sendLogoutPage(HttpServletResponse response) throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+        out.print("{\"redirectUrl\": \"" + frontRedirectUrl + "/logout\"}");
+        out.flush();
     }
 }
